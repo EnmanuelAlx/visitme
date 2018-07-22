8e9cc13e3a1715b93c38fa9119301266bc170151
joandv <joandv.gil@gmail.com>
2018-07-22 19:53:22 -0400

    Addition modal double binding fixed

--- src/visitme/js/custom/addModal.js
+++ src/visitme/js/custom/addModal.js
@@ -3,44 +3,51 @@
 //cancel on callback success
 // CLEAN FIELD
 const showAdditionModal = callback => {
-  if (!$("#addition-modal").exists()){
+  if (!$("#addition-modal").exists()) {
     const template = Handlebars.partials["addition-modal"];
     $("body").append(template());
+    listenClick();
+    listenSelect();
   }
 
   $("#addition-modal").modal("show");
-  const token = Sammy.apps.body.getAccessToken();
-  $("#user-select").select2({
-    ajax: {
-      url: `${MAIN_API}/user`,
-      dataType: "json",
-      delay: 250,
-      data: params => ({
-        query: params.term,
-        page: params.page
-      }),
-      headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    },
-    results: function(data, params) {
-      params.page = params.page || 1;
-      return {
-        results: data,
-        pagination: {
-          more: params.page * 30 < data.length
+
+
+  function listenSelect() {
+    const token = Sammy.apps.body.getAccessToken();
+    $("#user-select").select2({
+      ajax: {
+        url: `${MAIN_API}/user`,
+        dataType: "json",
+        delay: 250,
+        data: params => ({
+          query: params.term,
+          page: params.page
+        }),
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`
         }
-      };
-    },
-    placeholder: "Buscar un usuario",
-    escapeMarkup: function(markup) {
-      return markup;
-    },
-    minimumInputLength: 3,
-    templateResult: formatRepo,
-    templateSelection: formatRepoSelection
-  });
+      },
+      results: function (data, params) {
+        params.page = params.page || 1;
+        return {
+          results: data,
+          pagination: {
+            more: params.page * 30 < data.length
+          }
+        };
+      },
+      placeholder: "Buscar un usuario",
+      escapeMarkup: function (markup) {
+        return markup;
+      },
+      minimumInputLength: 3,
+      templateResult: formatRepo,
+      templateSelection: formatRepoSelection
+    });
+  };
+
 
   function formatRepo(user) {
     if (user.loading) {
@@ -52,21 +59,24 @@ const showAdditionModal = callback => {
   function formatRepoSelection(user) {
     return user.name;
   }
-  $(".addition-cb").one("click", async event => {
-    const user = $("#user-select").select2("data")[0];
-    const form = $(event.target.form);
-    const result = form.serializeJSON();
-    result.user = user;
-    form.trigger("reset");
-    startPreload("#addition-modal");
 
-    const res = await callback(result);
-    if (res){
+  function listenClick() {
+    $(".addition-cb").click(async event => {
+      const user = $("#user-select").select2("data")[0];
+      const form = $(event.target.form);
+      const result = form.serializeJSON();
+      result.user = user;
+      form.trigger("reset");
+      startPreload("#addition-modal");
+
+      const res = await callback(result);
       stopPreload();
-      toastr.error("Ocurrió un error al agregar", "Error");
-    }else{
-      $("#addition-modal").modal("hide");
-      toastr.info("Solicitud exitosa", "Éxito");
-    }
-  });
+      if (res) {
+        toastr.error("Ocurrió un error al agregar", "Error");
+      } else {
+        $("#addition-modal").modal("hide");
+        toastr.info("Solicitud exitosa", "Éxito");
+      }
+    });
+  };
 };
