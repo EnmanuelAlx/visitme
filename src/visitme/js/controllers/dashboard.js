1866395b5722212a0ce59f05153b159af70803d5
Jesus Mota <jesus.mota@rappi.com>
2018-07-22 19:11:26 -0500

    added stats fetch

--- src/visitme/js/controllers/dashboard.js
+++ src/visitme/js/controllers/dashboard.js
@@ -6,25 +6,21 @@
   const TEMPLATE_NAME = "dashboard";
   const HB = MyApp; // handlebars;
 
-
   app.get(ROOT, async context => {
     if (!app.getAccessToken()) return context.redirect("#/login");
     const { userType } = getSessionData();
 
-    if(userType === "RESIDENT")
-      return context.redirect("#/visits");
+    if (userType === "RESIDENT") return context.redirect("#/visits");
 
-    if (userType === "SECURITY")
-      return context.redirect("#/security");
+    if (userType === "SECURITY") return context.redirect("#/security");
 
     app.runRoute("put", "#/dashboard");
   });
 
-
-  app.put(ROOT, () => {
+  app.put(ROOT, async () => {
     const template = HB.templates[TEMPLATE_NAME];
     loadTemplate(CONTAINER, TEMPLATE_NAME, template());
-    initDashboard();
+    await initDashboard();
   });
 
   app.post("#/add-community", context => {
@@ -54,27 +50,31 @@
         await delay(500);
         location.reload();
       })
-      .catch(e=>{
+      .catch(e => {
         stopPreload();
         toastr.error(e.responseJSON.error.name, "Error");
       });
   });
 
-
   app.post("#/join-community", context => {
     const { community, reference } = $(context.target).serializeJSON();
-    const joinCommunity = postMainApi({ reference }, `communities/${community}/join`);
+    const joinCommunity = postMainApi(
+      { reference },
+      `communities/${community}/join`
+    );
     startPreload("#join-community-form");
     const name = $("#join-community-select option:selected").text();
     joinCommunity
       .then(() => {
         stopPreload();
-        app.store.set("communities", [{
-          name,
-          selected: true,
-          status: "pending",
-          kind: "RESIDENT"
-        }]);
+        app.store.set("communities", [
+          {
+            name,
+            selected: true,
+            status: "pending",
+            kind: "RESIDENT"
+          }
+        ]);
         app.store.set("userType", "RESIDENT");
         toastr.info("Te has unido exitosamente a la comunidad");
         context.redirect("#/unverified");
@@ -85,14 +85,13 @@
       });
   });
 
-
   app.post("#/select-community", context => {
     const { community } = $(context.target).serializeJSON();
     let { communities } = getSessionData();
     let name;
     communities = communities.map(comm => {
       comm.selected = false;
-      if (comm._id === community){
+      if (comm._id === community) {
         name = comm.name;
         comm.selected = true;
         app.store.set("userType", comm.kind);
@@ -105,52 +104,71 @@
     location.reload();
   });
 
-  const initCharts = () => {
+  const initCharts = async () => {
+    const { communities } = getSessionData();
+    const community = communities.find(comm => comm.selected === true)._id;
+    await loadHourChart(community);
+    await loadTypeChart(community);
+    await loadWeeksChart(community);
+  };
+
+  const initDashboard = async () => {
+    await initCharts();
+  };
+
+  const loadHourChart = async community => {
+    const data = await getMainApi(
+      {},
+      `communities/stats/${community}/visitByPartOfDay?month=1`
+    );
     new Chart($("#visits-hour"), {
       type: "bar",
       data: {
-        datasets: [{
-          backgroundColor: [
-            "#209ECE",
-            "#FF4A55",
-            "#ff9f1c"
-          ],
-          hoverBackgroundColor: [
-            "#209ECE",
-            "#FF4A55",
-            "#ff9f1c"
-          ],
-          data: [15, 25, 50, 52]
-        }],
-        labels: ["Mañana", "Tarde", "Noche"]
+        datasets: [
+          {
+            backgroundColor: ["#209ECE", "#FF4A55", "#ff9f1c"],
+            hoverBackgroundColor: ["#209ECE", "#FF4A55", "#ff9f1c"],
+            data
+          }
+        ],
+        labels: ["Mañana", "Tarde", "Noche", "Todo el dia", "Sin Especificar"]
       },
-      options:{
+      options: {
         legend: {
-          display: false,
+          display: false
         }
       }
     });
+  };
+
+  const loadTypeChart = async community => {
+    const data = await getMainApi(
+      {},
+      `communities/stats/${community}/visitsByType?month=1`
+    );
     new Chart($("#visits-type"), {
       type: "pie",
       data: {
-        datasets: [{
-          backgroundColor: [
-            "#209ECE",
-            "#FF4A55",
-            "#ff9f1c",
-            "#2ec4b6",
-            "#046589"
-          ],
-          hoverBackgroundColor: [
-            "#209ECE",
-            "#FF4A55",
-            "#ff9f1c",
-            "#2ec4b6",
-            "#046589"
-          ],
-          data: [15, 25, 50, 52]
-        }],
-        labels: ["Esperada", "Inesperada", "Esporádica", "Recurrente"]
+        datasets: [
+          {
+            backgroundColor: [
+              "#209ECE",
+              "#FF4A55",
+              "#ff9f1c",
+              "#2ec4b6",
+              "#046589"
+            ],
+            hoverBackgroundColor: [
+              "#209ECE",
+              "#FF4A55",
+              "#ff9f1c",
+              "#2ec4b6",
+              "#046589"
+            ],
+            data
+          }
+        ],
+        labels: ["Esperada", "Frecuente", "Esporádica", "No Esperada"]
       },
       options: {
         elements: {
@@ -159,30 +177,40 @@
           }
         },
         legend: {
-          display: false,
+          display: false
         },
         pieceLabel: {
           render: "percentage",
           fontColor: "#FFF",
-          precision: 2,
+          precision: 2
         },
         tooltips: {
-          mode: "label",
+          mode: "label"
         }
-      }});
+      }
+    });
+  };
+
+  const loadWeeksChart = async community => {
+    const data = await getMainApi(
+      {},
+      `communities/stats/${community}/allVisitsByMonth?month=1`
+    );
     new Chart($("#visits"), {
       type: "line",
       data: {
         labels: ["Semana 1", "Semana 2", "Semana 3", "Semana 4"],
-        datasets: [{
-          pointRadius: 0,
-          label: false,
-          data: [78, 60, 79, 15],
-          borderColor: "#20a0ce",
-          backgroundColor: "#20a0ce",
-          borderWidth: 3,
-          lineTension: 0
-        }]
+        datasets: [
+          {
+            pointRadius: 0,
+            label: false,
+            data,
+            borderColor: "#20a0ce",
+            backgroundColor: "#20a0ce",
+            borderWidth: 3,
+            lineTension: 0
+          }
+        ]
       },
       options: {
         elements: {
@@ -195,28 +223,32 @@
           display: false
         },
         scales: {
-          xAxes: [{
-            gridLines: {
-              display: false
+          xAxes: [
+            {
+              gridLines: {
+                display: false
+              }
             }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true, // minimum value will be 0.
-              stepsize: 2,
-              maxTicksLimit: 5,
-              max: 100,
-              callback: value => {
-                if (Number.isInteger(value)) {
-                  return value;
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true, // minimum value will be 0.
+                stepsize: 2,
+                maxTicksLimit: 5,
+                max: 100,
+                callback: value => {
+                  if (Number.isInteger(value)) {
+                    return value;
+                  }
                 }
               }
             }
-          }]
+          ]
         },
         tooltips: {
           callbacks: {
-            label: function (tooltipItem) {
+            label: function(tooltipItem) {
               return tooltipItem.yLabel;
             }
           }
@@ -224,10 +256,4 @@
       }
     });
   };
-
-
-  const initDashboard = () => {
-    initCharts();
-  };
-
 })();
